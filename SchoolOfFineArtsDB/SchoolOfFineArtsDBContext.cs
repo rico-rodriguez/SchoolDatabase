9d2aed779d5802ff49fc9424b5e20ff6b77974cd
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2022-10-25 23:23:27 +0900

    Data model set w/ seed teacher data

--- SchoolOfFineArtsDB/SchoolOfFineArtsDBContext.cs
+++ SchoolOfFineArtsDB/SchoolOfFineArtsDBContext.cs
@@ -1,9 +1,48 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SchoolOfFineArtsModels;
 
 namespace SchoolOfFineArtsDB
 {
     public class SchoolOfFineArtsDBContext : DbContext
     {
+        public DbSet<Teacher> Teachers { get; set; }
+        public SchoolOfFineArtsDBContext()
+        {
 
+        }
+        public SchoolOfFineArtsDBContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        //add to allow migrations when the context is not built
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                var config = builder.Build();
+                var cnstr = config["ConnectionStrings:SchoolOfFineArtsDB"];
+                var options = new DbContextOptionsBuilder<SchoolOfFineArtsDBContext>().UseSqlServer(cnstr);
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Teacher>(x =>
+            {
+                x.HasData(
+                    new Teacher() { Id = 1, FirstName = "Anne", LastName = "Sullivan", Age = 27 },
+                    new Teacher() { Id = 2, FirstName = "Maria", LastName = "Montessori", Age = 32 },
+                    new Teacher() { Id = 3, FirstName = "William", LastName = "McGuffey", Age = 21 },
+                    new Teacher() { Id = 4, FirstName = "Emma", LastName = "Willard", Age = 47 },
+                    new Teacher() { Id = 5, FirstName = "Jaime", LastName = "Escalante", Age = 62 }
+                );
+            });
+        }
     }
 }
--- SchoolOfFineArtsModels/Teacher.cs
+++ SchoolOfFineArtsModels/Teacher.cs
@@ -1,11 +1,21 @@
-﻿namespace SchoolOfFineArtsModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolOfFineArtsModels
 {
     public class Teacher
     {
-
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [Required, Range(1,130)]
         public int Age { get; set; }
 
 
